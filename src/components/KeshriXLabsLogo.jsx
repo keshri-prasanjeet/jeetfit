aff6abadf8a96866671ba5cdeaec6614d1f967f2
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-10-06 16:32:00 +0530

    moved the images to public

--- src/components/KeshriXLabsLogo.jsx
+++ src/components/KeshriXLabsLogo.jsx
@@ -4,7 +4,7 @@ const KeshriXLabsLogo = () => {
     return (
         <div className="absolute top-2 left-2">
             <img
-                src="src/assets/KeshriXLabs_expanded_logo.png"
+                src="../../public/KeshriXLabs_expanded_logo.png"
                 alt="KeshriXLabs logo"
                 className="w-32 h-auto sm:w-40 md:w-54 lg:w-64"
             />

79c91c19602d8fee4468b43ca6d34e095877a418
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-10-06 16:46:17 +0530

    fixed image reference and turned back fits to fit

--- src/components/KeshriXLabsLogo.jsx
+++ src/components/KeshriXLabsLogo.jsx
@@ -4,7 +4,7 @@ const KeshriXLabsLogo = () => {
     return (
         <div className="absolute top-2 left-2">
             <img
-                src="../../public/KeshriXLabs_expanded_logo.png"
+                src="/KeshriXLabs_expanded_logo.png" // Correct path from public folder
                 alt="KeshriXLabs logo"
                 className="w-32 h-auto sm:w-40 md:w-54 lg:w-64"
             />
